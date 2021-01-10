d7f0964c5c26eae1f103d8807e384bd0d3bcf8c3
Karol Sitarz <ka.sitarz@gmail.com>
2021-01-10 20:34:47 +0100

    rollback repos return type remove ts-mongoose

--- src/Repositories/BaseRepo.ts
+++ src/Repositories/BaseRepo.ts
@@ -1,16 +1,16 @@
 // definition for all Repositories
 
-import {Document, FilterQuery, Model, UpdateQuery} from 'mongoose';
+import {Document, FilterQuery, Model, Query, UpdateQuery} from 'mongoose';
 
 export interface Write<T extends Document> {
     create(item: T): Promise<T>;
-    update(id: string, item: UpdateQuery<T>): Promise<T>;
-    delete(id: string): Promise<T>;
+    update(id: string, item: UpdateQuery<T>): Query<T, T>;
+    delete(id: string): Query<T, T>;
 }
 
 export interface Read<T extends Document> {
-    find(item: FilterQuery<T>): Promise<T[]>;
-    findOne(item: FilterQuery<T>): Promise<T>;
+    find(item: FilterQuery<T>): Query<T[], T>;
+    findOne(item: FilterQuery<T>): Query<T, T>;
 }
 
 export class BaseRepository<T extends Document> implements Write<T>, Read<T> {
@@ -23,19 +23,19 @@ export class BaseRepository<T extends Document> implements Write<T>, Read<T> {
         return this.model.create(item)
     }
 
-    update(id: string, item: UpdateQuery<T>): Promise<T> {
+    update(id: string, item: UpdateQuery<T>): Query<T, T> {
         return this.model.findByIdAndUpdate(id, item)
     }
 
-    delete(id: string): Promise<T> {
+    delete(id: string): Query<T, T> {
         return this.model.findByIdAndDelete(id)
     }
 
-    find(item: FilterQuery<T>): Promise<T[]> {
+    find(item: FilterQuery<T>): Query<T[], T> {
         return this.model.find(item)
     }
 
-    findOne(item: FilterQuery<T>): Promise<T> {
+    findOne(item: FilterQuery<T>): Query<T, T> {
         return this.model.findOne(item)
     }
 
